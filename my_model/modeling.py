1790e8670e3ec1b580de7638631a872b6976d7f2
MariaPau03 <mppijoan@gmail.com>
2026-02-20 15:40:54 +0100

    .

--- my_model/modeling.py
+++ my_model/modeling.py
@@ -10,7 +10,7 @@ env.io.atom_files_directory = ['.', '../atom_files']
 
 a = automodel(env,
               alnfile  = 'final_alignment.pir', # alignment filename
-              knowns   = ('1sbhA', '1yjaA', '1yjbA'),     # codes of the templates
+              knowns   = ('1sbhA', '1s01A', '1sbiA'),     # codes of the templates
               sequence = 'P11018')               # code of the target
 a.starting_model= 1                 # index of the first model
 a.ending_model  = 2                 # index of the last model
---
+++ my_model/see_energy_prosa.cmd
@@ -0,0 +1,13 @@
+read pdb model1.pdb mod1
+read pdb model2.pdb mod2
+read pdb 1sbhA.pdb 1sbh
+read pdb 1yjaA.pdb 1yja
+read pdb 1yjbA.pdb 1yjb
+analyse energy *
+winsize * 30
+color * mod1 blue
+color * mod2 cyan
+color * 1sbh green
+color * 1yja red
+color * 1yjb white
+plot
