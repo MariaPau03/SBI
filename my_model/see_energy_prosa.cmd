b386487ce4ee733f2ba1c198846d27fe5456c65d
MariaPau03 <mppijoan@gmail.com>
2026-02-21 12:28:46 +0100

    added files in which I modified the seq :), to enhance the energy of the model

--- my_model/see_energy_prosa.cmd
+++ my_model/see_energy_prosa.cmd
@@ -1,3 +1,4 @@
+read pdb P11018.BL00040003.pdb mod1_modif
 read pdb model1.pdb mod1
 read pdb model2.pdb mod2
 read pdb 1sbhA.pdb 1sbh
@@ -5,6 +6,7 @@ read pdb 1yjaA.pdb 1yja
 read pdb 1yjbA.pdb 1yjb
 analyse energy *
 winsize * 30
+color * mod1_modif magenta
 color * mod1 blue
 color * mod2 cyan
 color * 1sbh green
