fa92121f8433a8e3e0866d3c1b80e88a8ac91425
MariaPau03 <mppijoan@gmail.com>
2026-03-03 20:33:55 +0100

    checked problem1

---
+++ problemes/problem_1_/alignment.pir
@@ -0,0 +1,6 @@
+>P1;problem_1_A
+structureX:problem_1_A:1: : 21  : :  : : -1.00 :-1.00
+KIAMLGEGGVGEAALTVNLTK*
+>P1;problem_1_A2
+structureX:problem_1_A2:1: : 21  : :  : : -1.00 :-1.00
+KIAMLGEGGVGEAALTVNLTK*
---
+++ problemes/problem_1_/model_default.py
@@ -0,0 +1,18 @@
+# Comparative modeling by the automodel class
+from modeller import *              # Load standard Modeller classes
+from modeller.automodel import *    # Load the automodel class
+
+log.verbose()    # request verbose output
+env = environ()  # create a new MODELLER environment to build this model in
+
+# directories for input atom files
+env.io.atom_files_directory = ['.', '../atom_files']
+
+a = automodel(env,
+              alnfile  = 'alignment.pir',     # alignment filename
+              knowns   = 'problem_1_A',              # codes of the templates
+              sequence = 'problem_1_A2')              # code of the target
+a.starting_model= 1                 # index of the first model
+a.ending_model  = 1                 # index of the last model
+                                    # (determines how many models to calculate)
+a.make()                            # do the actual comparative modeling
